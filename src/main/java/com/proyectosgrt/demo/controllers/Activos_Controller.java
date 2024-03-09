5bd711a6c3c9438e09e0f9288677007560ba0a92
Deibyth <alejopadilla0322@gmail.com>
2024-03-08 21:29:12 -0500

    1er Commit

--- src/main/java/com/proyectosgrt/demo/controllers/Activos_Controller.java
+++ src/main/java/com/proyectosgrt/demo/controllers/Activos_Controller.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,22 +20,39 @@ public class Activos_Controller {
     @Autowired
     private Repository_Activos rep;
 
+    //Consulta Generica
     @GetMapping("/act")
     public String incio() {
         return "Conect";
     }
 
+    //Consulta Tabla
     @GetMapping("/acts")
     public List<Activos> getActivos() {
         return rep.findAll();
     }
 
+    //Creacion Activo
     @SuppressWarnings("null")
     @PostMapping("/add_act")
     public String add_act(@RequestBody Activos ac) {
             rep.save(ac);
             return "Active Add";
     }
+
+    //Editar Usuario
+    @PutMapping("/edit_act/{idserial}")
+    public String edit_act(@PathVariable String idserial, @RequestBody Activos ac) {
+        @SuppressWarnings("null")
+        Activos actualizarActivos = rep.findById(idserial).get();
+        actualizarActivos.setModelo(ac.getModelo());
+        actualizarActivos.setIdtipo(ac.getIdtipo());
+        actualizarActivos.setIdmarca(ac.getIdmarca());
+        rep.save(actualizarActivos);
+        return "Activo editado";
+        
+    }
+
 }
 
 
