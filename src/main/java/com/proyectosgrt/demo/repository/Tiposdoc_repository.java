e96cb5c69dc6379f1055f58f12f22803d4a9cb38
Cristian Mendoza <cristian.mendozapds@gmail.com>
2024-03-08 19:44:43 -0500

    Delete Tiposdoc y Departamento

--- src/main/java/com/proyectosgrt/demo/repository/Tiposdoc_repository.java
+++
@@ -1,9 +0,0 @@
-package com.proyectosgrt.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyectosgrt.demo.models.Tiposdoc;
-
-public interface Tiposdoc_repository extends JpaRepository <Tiposdoc, Byte> {
-
-}
