a6ab34d8975d8c6e383ea93d947b5f4badef78b4
fredy100100 <fredyandresruiz9@gmail.com>
2024-03-12 21:21:28 -0500

    Filtro de solicitud por rango de fecha

--- src/main/java/com/proyectosgrt/demo/controllers/Solicitudes_controller.java
+++ src/main/java/com/proyectosgrt/demo/controllers/Solicitudes_controller.java
@@ -1,8 +1,12 @@
 package com.proyectosgrt.demo.controllers;
 
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +16,8 @@ import com.proyectosgrt.demo.models.Solicitudes;
 import com.proyectosgrt.demo.repository.Solicitudes_Repository;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 
 @RestController
@@ -36,12 +42,39 @@ public class Solicitudes_controller {
             return "Solicitud Create";
             
 }
-@PutMapping("Cerrar/{idsol}")
+@PutMapping("cerrar/{idsol}")
 public String actulizar(@PathVariable Long idsol, @RequestBody Solicitudes so) {
  
     @SuppressWarnings("null")
     Solicitudes actualizar_estado = repo.findById(idsol).get();
-    actualizar_estado.setIdest(so.getIdcat());
+    actualizar_estado.setIdest(so.getIdest());
+    repo.save(actualizar_estado);
     return "Estado de soliictud actualizado";
 }
+
+
+@PutMapping("/modificar/{idsol}")
+public String actualizarsol (@PathVariable long idsol, @RequestBody Solicitudes so){
+    Solicitudes actualizarsol = repo.findById(idsol).get();
+    actualizarsol.setFechaci(so.getFechaci());
+    actualizarsol.setPrio(so.getPrio());
+    actualizarsol.setObser(so.getObser());
+    actualizarsol.setDiag(so.getDiag());
+    actualizarsol.setIdest(so.getIdest());
+    actualizarsol.setNodoccliente(so.getNodoccliente());
+    actualizarsol.setNodoctecnico(so.getNodoctecnico());
+    actualizarsol.setIdcat(so.getIdcat());
+    repo.save(actualizarsol);
+    return "Solicitud actualizada";
+}
+
+@GetMapping("/solicitud_fecha")
+    public List <Solicitudes> Solicitudes() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+    LocalDateTime f1 = LocalDateTime.parse("2024-03-10T03:08:17.000+00:00", formatter);
+    LocalDateTime f2 = LocalDateTime.parse("2024-03-15T03:08:17.000+00:00", formatter);
+        return repo.getfech(f1, f2);
+}
+
 }
--- src/main/java/com/proyectosgrt/demo/repository/Solicitudes_Repository.java
+++ src/main/java/com/proyectosgrt/demo/repository/Solicitudes_Repository.java
@@ -1,9 +1,16 @@
 package com.proyectosgrt.demo.repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.proyectosgrt.demo.models.Solicitudes;
 
 public interface Solicitudes_Repository extends JpaRepository<Solicitudes, Long> {
 
+    @Query("SELECT s FROM Solicitudes s WHERE s.fechacre BETWEEN ?1 AND ?2")
+    List<Solicitudes> getfech (LocalDateTime f1, LocalDateTime f2);
 }
