49c76f90f3cb59d2c778d5e33eccb88ab29cf75e
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2024-03-12 21:09:31 -0500

    Deleted ActivoPersonas

--- src/main/java/com/proyectosgrt/demo/repository/Repository_ActivosPersonas.java
+++
@@ -1,9 +0,0 @@
-package com.proyectosgrt.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyectosgrt.demo.models.ActivosPersonas;
-
-public interface Repository_ActivosPersonas extends JpaRepository <ActivosPersonas, Integer>{
-
-}
