14dc87e1797a54fa12cc0eb1c2fed8d39654b8e2
Cristian Mendoza <cristian.mendozapds@gmail.com>
2024-03-15 21:39:29 -0500

    ajuste

--- src/main/java/com/proyectosgrt/demo/controllers/Asignaciones_controller.java
+++ src/main/java/com/proyectosgrt/demo/controllers/Asignaciones_controller.java
@@ -2,16 +2,13 @@ package com.proyectosgrt.demo.controllers;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.proyectosgrt.demo.models.Activos;
 import com.proyectosgrt.demo.models.Asignaciones;
-import com.proyectosgrt.demo.models.Personas;
 import com.proyectosgrt.demo.repository.Repository_Asignaciones;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
