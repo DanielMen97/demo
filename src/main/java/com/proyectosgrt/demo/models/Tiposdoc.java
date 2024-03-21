2d7651feab0d90b36086737d072c124deffbe7d4
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2024-03-20 23:15:53 -0500

    tiposdoc creado

---
+++ src/main/java/com/proyectosgrt/demo/models/Tiposdoc.java
@@ -0,0 +1,19 @@
+package com.proyectosgrt.demo.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+public class Tiposdoc {
+
+  @Id
+  @Getter
+  @Setter
+  private Integer iddoc;
+
+  @Column
+  @Getter
+  @Setter
+  private String tipo;
+
+}
