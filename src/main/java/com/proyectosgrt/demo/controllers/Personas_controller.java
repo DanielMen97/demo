cede93c6d4015b22407b92fb0cc2534ba8fe47bc
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2024-03-26 00:04:30 -0500

    Crear metodo consultar usuario por id

--- src/main/java/com/proyectosgrt/demo/controllers/Personas_controller.java
+++ src/main/java/com/proyectosgrt/demo/controllers/Personas_controller.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import com.proyectosgrt.demo.models.Personas;
 import com.proyectosgrt.demo.repository.Repository_Personas;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Personas_controller {
@@ -26,69 +27,73 @@ public class Personas_controller {
   private Repository_Personas rep;
 
   @GetMapping("")
-    public String incio() {
-        return "Conect";
-    }
-  @CrossOrigin(origins = "http://localhost:3000/")  
+  public String incio() {
+    return "Conect";
+  }
+
+  @CrossOrigin(origins = "http://localhost:3000/")
   @GetMapping("/users")
-    public List<Personas> getUsuario() {
-        return rep.findAll();
+  public List<Personas> getUsuario() {
+    return rep.findAll();
+  }
+
+  @SuppressWarnings("null")
+  @PostMapping("/create_user")
+  public String create_user(@RequestBody Personas us) {
+    rep.save(us);
+    return "User Create";
+  }
+
+  // Crea un usuario validando documento
+  @CrossOrigin(origins = "http://localhost:3000/")
+  @PostMapping("/create")
+  public ResponseEntity<Object> create(@RequestBody Personas us) {
+    Optional<Personas> res = rep.findPersonasBynodoc(us.getNodoc());
+    Optional<Personas> resc = rep.findByCorreo(us.getCorreo());
+    HashMap<String, Object> datos = new HashMap<>();
+
+    if (res.isPresent()) {
+      datos.put("error", true);
+      datos.put("mensaje", "El usuario ya esta creado");
+      return new ResponseEntity<>(datos, HttpStatus.CONFLICT);
+    } else if (resc.isPresent()) {
+      datos.put("error", true);
+      datos.put("mensaje", "El correo ya se encuentra registrado");
+      return new ResponseEntity<>(datos, HttpStatus.CONFLICT);
     }
+    rep.save(us);
+    datos.put("data", us);
+    datos.put("mensaje", "El usuario fue creado correctamente");
+    return new ResponseEntity<>(datos, HttpStatus.CREATED);
+  }
+
+  @CrossOrigin(origins = "http://localhost:3000/")
+  @PutMapping("edituser/{nodoc}")
+  public String editUser(@PathVariable String nodoc, @RequestBody Personas us) {
     @SuppressWarnings("null")
-    @PostMapping("/create_user")
-    public String create_user(@RequestBody Personas us) {
-            rep.save(us);
-            return "User Create";
-    }
+    Personas actualizarUsuario = rep.findById(nodoc).get();
+    actualizarUsuario.setPnom(us.getPnom());
+    actualizarUsuario.setSnom(us.getSnom());
+    actualizarUsuario.setPape(us.getPape());
+    actualizarUsuario.setSape(us.getSape());
+    actualizarUsuario.setCorreo(us.getCorreo());
+    actualizarUsuario.setCelular(us.getCelular());
+    actualizarUsuario.setPass(us.getPass());
+    actualizarUsuario.setIdrol(us.getIdrol());
+    actualizarUsuario.setIdcargo(us.getIdcargo());
+    actualizarUsuario.setIdsede(us.getIdsede());
+    actualizarUsuario.setIddoc(us.getIddoc());
+    actualizarUsuario.setEstado(us.isEstado());
+    rep.save(actualizarUsuario);
 
-    //Crea un usuario validando documento
-    @CrossOrigin(origins = "http://localhost:3000/")  
-    @PostMapping("/create")
-    public ResponseEntity<Object> create(@RequestBody Personas us) {
-            Optional<Personas> res = rep.findPersonasBynodoc(us.getNodoc());
-            Optional<Personas> resc = rep.findByCorreo(us.getCorreo());
-            HashMap<String, Object> datos = new HashMap<>();
+    return "Información de usuario Actualizado";
+  }
 
-              if(res.isPresent()) {
-                datos.put("error", true); 
-                datos.put("mensaje", "El usuario ya esta creado");
-                return new ResponseEntity<>(datos,HttpStatus.CONFLICT);
-              } else if (resc.isPresent()) {
-                datos.put("error", true); 
-                datos.put("mensaje", "El correo ya se encuentra registrado");
-                return new ResponseEntity<>(datos,HttpStatus.CONFLICT);
-              }
-              rep.save(us); 
-              datos.put("data", us);
-              datos.put("mensaje", "El usuario fue creado correctamente");
-              return new ResponseEntity<>(datos,HttpStatus.CREATED);
-            }
+  @CrossOrigin(origins = "http://localhost:3000/")
+  @GetMapping("/userbynodoc/{nodoc}")
+  @SuppressWarnings("null")
+  public Personas userByNodoc(@PathVariable String nodoc) {
+    return rep.findById(nodoc).get();
+  }
 
-    @PutMapping("edituser/{nodoc}")
-    public String editUser(@PathVariable String nodoc, @RequestBody Personas us) {
-        @SuppressWarnings("null")
-        Personas actualizarUsuario = rep.findById(nodoc).get();
-        actualizarUsuario.setPnom(us.getPnom());
-        actualizarUsuario.setSnom(us.getSnom());
-        actualizarUsuario.setPape(us.getPape());
-        actualizarUsuario.setSape(us.getSape());
-        actualizarUsuario.setCorreo(us.getCorreo());
-        actualizarUsuario.setCelular(us.getCelular());
-        actualizarUsuario.setPass(us.getPass());
-        actualizarUsuario.setIdrol(us.getIdrol());
-        actualizarUsuario.setIdcargo(us.getIdcargo());
-        actualizarUsuario.setIdsede(us.getIdsede());
-        actualizarUsuario.setIddoc(us.getIddoc());
-        actualizarUsuario.setEstado(us.isEstado());
-        rep.save(actualizarUsuario);
-
-        return "Información de usuario Actualizado";
-    }
-    }
-    
-        
-           
-
-
-
-
+}
