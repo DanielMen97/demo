370e67e3933bbf46e9685114dbeefa9b1848e5ce
DanielMen97 <cristian.mendozapds@gmail.com>
2024-06-20 16:58:11 -0500

    Tiposdoc DTO

--- src/main/java/com/proyectosgrt/demo/controllers/Tiposdoc_Controller.java
+++ src/main/java/com/proyectosgrt/demo/controllers/Tiposdoc_Controller.java
@@ -4,24 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.proyectosgrt.demo.models.Tiposdoc;
-import com.proyectosgrt.demo.repository.Repository_Tiposdoc;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.proyectosgrt.demo.DTO.TiposdocDTO;
+import com.proyectosgrt.demo.services.TiposdocServices;
 
 @RestController
 public class Tiposdoc_Controller {
 
   @Autowired
-  private Repository_Tiposdoc tip;
+  TiposdocServices tiposdocServices;
 
-  @CrossOrigin(origins = "http://localhost:3000/") 
-  @GetMapping("/tiposdoc")
-  public List<Tiposdoc> listTiposdoc() {
-      return tip.findAll();
+  @GetMapping("/admin/tiposdoc")
+  public List<TiposdocDTO> getListTiposdoc() {
+      return tiposdocServices.getTiposdoc();
   }
-  
 }
